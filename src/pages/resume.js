d072de2854332736cceaa7d02b3303b1ce0eecb9
Andrew Derse <aderse@back2back.org>
2023-08-06 04:58:36 -0500

    updating resume.js

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,9 +4,9 @@ export function ResumeMain() {
     return(
         <main>
             <section id="sub-header">
-                <div class="container-fluid">
-                    <div class="row mb-5">
-                        <div class="col-12 text-center p-5">
+                <div className="container-fluid">
+                    <div className="row mb-5">
+                        <div className="col-12 text-center p-5">
                             <h2>Resume</h2>
                             <p>Keep moving forward</p>
                         </div>
@@ -14,56 +14,77 @@ export function ResumeMain() {
                 </div>
             </section>
             <section id="resume">
-                <div class="container">
-                    <div class="row">
-                        <h3 class="ml-3">About Me</h3>
+                <div className="container">
+                    <div className="row">
+                        <h3 className="ml-3">About Me</h3>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>Professional</h5>
                         </div>
-                        <div class="col-sm-12 col-md-9">
+                        <div className="col-sm-12 col-md-9">
                             <p>Passionate about leadership and developing leaders who bring the latest technologies to our clients.</p>
                             <p>Quickly learn and master new technologies; equally successful in both team and self-directed settings;
                                 proficient in a range of computer systems, languages, tools and testing methodologies.</p>
                         </div>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>Personal</h5>
                         </div>
-                        <div class="col-sm-12 col-md-9">
+                        <div className="col-sm-12 col-md-9">
                             <p>I am a loving husband and father, who is passionate about defending the orphans and widows.</p>
+                            <p>I also like writing joke books! You can learn more here: <a href="https://50dadjokes.com">50DadJokes.com</a></p>
                             <p>When I'm not sitting behind a computer, you can find me outside enjoying the beautiful and diverse nature
                                 of Wisconsin.</p>
                         </div>
                     </div>
-                    <div class="row">
-                        <h3 class="mt-5 ml-3">Work Experience</h3>
+                    <div className="row">
+                        <h3 className="mt-5 ml-3">Work Experience</h3>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
-                            <h5>APR 2017 - Present</h5>
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
+                            <h5>July 2022 - Present</h5>
+                            <p><a href="https://www.creedinteractive.com">www.creedinteractive.com</a></p>
+                        </div>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>Sr. Developer/Team Lead - <span
+                                className="company-name">Creed Interactive</span></h5>
+                            <p>
+                                I lead the Open Source web development team as we tackle website redesigns and builds.<br/>
+                                My team is proficient in WordPress, Drupal, Laravel, React, and more.<br/>
+                                Responsible for directing day to day work for developers and ensuring project completion on time and on budget.<br/>
+                                Responsible for hiring and retaining the best developers.<br/>
+                                Responsible for training and mentoring developers from Junior to Senior levels.<br/>
+                            </p>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
+                            <h5>APR 2017 - July 2022</h5>
                             <p><a href="https://www.madcitycoders.com">www.madcitycoders.com</a></p>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>Owner - <span class="company-name">Mad City Coders, LLC</span></h5>
-                            <p>At Mad City Coders, we provide web solutions for small, medium and large sized businesses with the focus
-                                on WordPress and custom PHP projects.</p>
-                            <p>We have successfully completed over 20 projects within the first 2 years of the business opening.</p>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>Owner - <span className="company-name">Mad City Coders, LLC</span></h5>
+                            <p>I successfully ran a website development agency for 5 years and eventually sold the company off to a friend.
+                                The experience was simply amazing and I enjoyed helping all of the businesses throughout that time.
+                                I have closed over 50 projects successfully, many of which are still in use today. Unfortunately, do to the
+                                nature of my current job, I am no longer able to able to run the business as it would be a conflict of interest.
+                            </p>
                         </div>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
-                            <h5>FEB 2018 - Present</h5>
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
+                            <h5>FEB 2018 - July 2022</h5>
                             <p><a href="https://www.fullcompass.com">www.fullcompass.com</a></p>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>Website Developer - Technical Lead - <span class="company-name">Full Compass Systems</span></h5>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>Website Developer - Technical Lead - <span className="company-name">Full Compass Systems</span></h5>
                             <p>
                                 Lead web development team in large and small projects.<br />
                                 Help make technical decisions with what direction team goes and what technologies we use.<br />
@@ -85,13 +106,13 @@ export function ResumeMain() {
                         </div>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>AUG 2016 - FEB 2018</h5>
                             <p><a href="https://www.sounddevices.com">www.sounddevices.com</a></p>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>IT Manager - Website Developer - <span class="company-name">Sound Devices</span></h5>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>IT Manager - Website Developer - <span className="company-name">Sound Devices</span></h5>
                             <p>
                                 Responsible for all aspects of IT. Maintain and build internal infrastructure of computers, servers,
                                 and network within a diverse OS environment (Mac, Windows, Linux).
@@ -103,30 +124,30 @@ export function ResumeMain() {
                         </div>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>JUN 2016 - AUG 2016</h5>
                             <p><a href="https://www.earthlinginteractive.com">www.earthlinginteractive.com</a></p>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>Developer - <span class="company-name">Earthling Interactive</span></h5>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>Developer - <span className="company-name">Earthling Interactive</span></h5>
                             <p>
-                                Responsible for developing and maintaining CodeIgnitor websites for Sparc project.
+                                Responsible for developing and maintaining CodeIgniter websites for Sparc project.
                             </p>
                             <p>
                                 Our focus was to create beautiful websites that help clients have an e-commerce presence through Magento,
-                                CodeIgnitor and/or WordPress.
+                                CodeIgniter and/or WordPress.
                             </p>
                         </div>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>MAR 2013 - MAR 2016</h5>
                             <p><a href="https://www.back2back.org">www.back2back.org</a></p>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>Missionary - House Parent - Developer - Finance Controller - <span class="company-name">Back2Back Ministries</span></h5>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>Missionary - House Parent - Developer - Finance Controller - <span className="company-name">Back2Back Ministries</span></h5>
                             <p>
                                 Oversaw direction of millions of dollars across several areas of ministry. Oversaw direction of finance employees as we
                                 helped the ministry distribute and collect money to keep operating at an efficient level.
@@ -147,13 +168,13 @@ export function ResumeMain() {
                         </div>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>FEB 2010 - JAN 2013</h5>
                             <p><a href="https://www.nukusa.com">www.nukusa.com</a></p>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>Network/User Support Analyst - Web Developer - <span class="company-name">NUK USA</span></h5>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>Network/User Support Analyst - Web Developer - <span className="company-name">NUK USA</span></h5>
                             <p>
                                 Responsible for turning paper-based routines into efficient web-based processes with email notifications and
                                 instant feedback. Implemented company Intranet, Certification Access Portal (saved the company $88,000 annually)
@@ -166,64 +187,64 @@ export function ResumeMain() {
                             </p>
                         </div>
                     </div>
-                    <div class="row">
-                        <h3 class="mt-5 ml-3">Education</h3>
+                    <div className="row">
+                        <h3 className="mt-5 ml-3">Education</h3>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>MAR 2020</h5>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>Certified Cloud Developer - Assoc <span class="company-name">Amazon Web Services</span></h5>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>Certified Cloud Developer - Assoc <span className="company-name">Amazon Web Services</span></h5>
                             <p>
                                 AWS Certified Cloud Developer - Assoc
                             </p>
                         </div>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>NOV 2017</h5>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>Certified Cloud Practitioner <span class="company-name">Amazon Web Services</span></h5>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>Certified Cloud Practitioner <span className="company-name">Amazon Web Services</span></h5>
                             <p>
                                 AWS Certified Cloud Practitioner
                             </p>
                         </div>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>JAN 2012</h5>
                             <p><a href="https://www.fvtc.edu">www.fvtc.edu</a></p>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>A.A.S. in IT - Programmer/Systems Analyst <span class="company-name">Fox Valley Technical College</span></h5>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>A.A.S. in IT - Programmer/Systems Analyst <span className="company-name">Fox Valley Technical College</span></h5>
                             <p>
                                 Specializing in Database and Website Development.
                             </p>
                         </div>
                     </div>
                     <hr />
-                    <div class="row">
-                        <div class="col-sm-12 col-md-3">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-3">
                             <h5>SEP 2009</h5>
                         </div>
-                        <div class="col-sm-12 col-md-9">
-                            <h5>CompTIA A+ <span class="company-name">CompTIA</span></h5>
+                        <div className="col-sm-12 col-md-9">
+                            <h5>CompTIA A+ <span className="company-name">CompTIA</span></h5>
                             <p>
                                 Certified CompTIA A+
                             </p>
                         </div>
                     </div>
-                    <div class="row">
-                        <h3 class="mt-5 ml-3">Skills</h3>
+                    <div className="row">
+                        <h3 className="mt-5 ml-3">Skills</h3>
                     </div>
                     <hr />
-                    <div class="text-center">
-                        <div class="skills">
+                    <div className="text-center">
+                        <div className="skills">
                             <button>AWS</button> <button>Lambda</button> <button>DynamoDB</button> <button>AWS S3</button> <button>Node.js</button> <button>Python</button> <button>PHP</button> <button>WordPress</button> <button>Joomla!</button>
                             <br />
                             <br />
